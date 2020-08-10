b2b0e59604b2466d43c3cdcbb81d7aa082cf731b
Gustavoctt <gustavocarrertartare@gmail.com>
2020-08-10 18:41:36 -0300

    Atualização do npm

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //IP da máquina com base no servidor Expo
-    baseURL: 'http://192.168.0.108:3333'
+    baseURL: 'http://192.168.0.109:3333'
 });
 
 export default api;
